c14ac41a8555efcb232b989c50516ea4ecfc7853
John Michael <john.parsons85210@gmail.com>
2017-08-11 16:59:34 -0700

    indent js

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,41 @@
 $(document).ready(function() {
 
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            return false;
-          } else {
-            $target.attr('tabindex','-1');
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            var $target = $(target);
             $target.focus();
-          };
-        });
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr('tabindex', '-1');
+              $target.focus();
+            };
+          });
+        }
       }
-    }
-  });
-
-
+    });
   $("form").submit(function(event) {
     var rubytotal = 0
     var phptotal = 0
     var csharptotal = 0
-
-
-
     $(".ruby").hide();
     $(".php").hide();
     $(".csharp").hide();
     $('#output').hide();
-
     event.preventDefault();
-
     var answer1 = $("input:radio[name=question1]:checked").val();
     var answer2 = $("input:radio[name=question2]:checked").val();
     var answer3 = $("input:radio[name=question3]:checked").val();
@@ -51,55 +43,87 @@ $('a[href*="#"]')
     var answer5 = $("input:radio[name=question5]:checked").val();
     // var mult1 = $("select:dropdown[name=mult]:selected").val();
     // console.log(mult1) will try again later
-      if (answer1 === "ruby1") {$(rubytotal =(parseInt(rubytotal)+1))};
-      if (answer1 === "php1") {$(phptotal =(parseInt(phptotal)+1))};
-      if (answer1 === "csharp1") {$(csharptotal =(parseInt(csharptotal)+1))};
+    if (answer1 === "ruby1") {
+      $(rubytotal = (parseInt(rubytotal) + 1))
+    };
+    if (answer1 === "php1") {
+      $(phptotal = (parseInt(phptotal) + 1))
+    };
+    if (answer1 === "csharp1") {
+      $(csharptotal = (parseInt(csharptotal) + 1))
+    };
 
-      if (answer2 === "ruby2") {$(rubytotal =(parseInt(rubytotal)+1))};
-      if (answer2 === "php2") {$(phptotal =(parseInt(phptotal)+1))};
-      if (answer2 === "csharp2") {$(csharptotal =(parseInt(csharptotal)+1))};
+    if (answer2 === "ruby2") {
+      $(rubytotal = (parseInt(rubytotal) + 1))
+    };
+    if (answer2 === "php2") {
+      $(phptotal = (parseInt(phptotal) + 1))
+    };
+    if (answer2 === "csharp2") {
+      $(csharptotal = (parseInt(csharptotal) + 1))
+    };
 
-      if (answer3 === "ruby3") {$(rubytotal =(parseInt(rubytotal)+1))};
-      if (answer3 === "php3") {$(phptotal =(parseInt(phptotal)+1))};
-      if (answer3 === "csharp3") {$(csharptotal =(parseInt(csharptotal)+1))};
+    if (answer3 === "ruby3") {
+      $(rubytotal = (parseInt(rubytotal) + 1))
+    };
+    if (answer3 === "php3") {
+      $(phptotal = (parseInt(phptotal) + 1))
+    };
+    if (answer3 === "csharp3") {
+      $(csharptotal = (parseInt(csharptotal) + 1))
+    };
 
-      if (answer4 === "ruby4") {$(rubytotal =(parseInt(rubytotal)+1))};
-      if (answer4 === "php4") {$(phptotal =(parseInt(phptotal)+1))};
-      if (answer4 === "csharp4") {$(csharptotal =(parseInt(csharptotal)+1))};
+    if (answer4 === "ruby4") {
+      $(rubytotal = (parseInt(rubytotal) + 1))
+    };
+    if (answer4 === "php4") {
+      $(phptotal = (parseInt(phptotal) + 1))
+    };
+    if (answer4 === "csharp4") {
+      $(csharptotal = (parseInt(csharptotal) + 1))
+    };
 
-      if (answer5 === "ruby5") {$(rubytotal =(parseInt(rubytotal)+10))};
-      if (answer5 === "php5") {$(phptotal =(parseInt(phptotal)+10))};
-      if (answer5 === "csharp5") {$(csharptotal =(parseInt(csharptotal)+10))};
+    if (answer5 === "ruby5") {
+      $(rubytotal = (parseInt(rubytotal) + 10))
+    };
+    if (answer5 === "php5") {
+      $(phptotal = (parseInt(phptotal) + 10))
+    };
+    if (answer5 === "csharp5") {
+      $(csharptotal = (parseInt(csharptotal) + 10))
+    };
 
-
-      // console.log(csharptotal + "csharp");
-      // console.log(rubytotal + "ruby");       Debugging
-      // console.log(phptotal + "php");
-
-
-
-    if (rubytotal === phptotal && rubytotal ===  csharptotal){
-      $('#output').toggle();
-      $("#output").text("It looks like you are undecided! Consult Sorting Hat or try the quiz again.");
-    }
-    if (rubytotal === phptotal && rubytotal > csharptotal){
-      $('#output').toggle();
-      $("#output").text("It looks like you you tied on Ruby/Rails and PHP/Drupal! ");
-    }
-    if (rubytotal === csharptotal && rubytotal > phptotal){
-      $('#output').toggle();
-      $("#output").text("It looks like you you tied on Ruby/Rails and C#! ");
-    }
-    if (phptotal === csharptotal && csharptotal > rubytotal){
-      $('#output').toggle();
-      $("#output").text("It looks like you you tied on PHP/Drupal and C#/.Net! ");
-    }
-    if (rubytotal > phptotal && rubytotal > csharptotal) {
-      $('.ruby').toggle();}
-    if (phptotal > csharptotal && phptotal > rubytotal) {
-      $('.php').toggle();}
-    if (csharptotal > rubytotal && csharptotal > phptotal) {
-      $('.csharp').toggle();}
-
+  });
 });
-});
+
+
+
+//  For improvements on functionality later.
+//
+//
+// if (rubytotal === phptotal && rubytotal ===  csharptotal){
+//   $('#output').toggle();
+//   $("#output").text("It looks like you are undecided! Consult Sorting Hat or try the quiz again.");
+// }
+// if (rubytotal === phptotal && rubytotal > csharptotal){
+//   $('#output').toggle();
+//   $("#output").text("It looks like you you tied on Ruby/Rails and PHP/Drupal! ");
+// }
+// if (rubytotal === csharptotal && rubytotal > phptotal){
+//   $('#output').toggle();
+//   $("#output").text("It looks like you you tied on Ruby/Rails and C#! ");
+// }
+// if (phptotal === csharptotal && csharptotal > rubytotal){
+//   $('#output').toggle();
+//   $("#output").text("It looks like you you tied on PHP/Drupal and C#/.Net! ");
+// }
+// if (rubytotal > phptotal && rubytotal > csharptotal) {
+//   $('.ruby').toggle();}
+// if (phptotal > csharptotal && phptotal > rubytotal) {
+//   $('.php').toggle();}
+// if (csharptotal > rubytotal && csharptotal > phptotal) {
+//   $('.csharp').toggle();}
+
+// console.log(csharptotal + "csharp");
+// console.log(rubytotal + "ruby");       Debugging
+// console.log(phptotal + "php");
