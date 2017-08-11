1f2e53faa5464cda5ca75e601d68b56fdcb43b1e
John Michael <john.parsons85210@gmail.com>
2017-08-11 09:51:21 -0700

    add basic outline of front and business end

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+ // identify answers as language 1, lan2, lan3...
+// totallan1 = how many times was lan1 selected * how strongly user feels about this question
+// if totallan1 > totallan2 > totallan3, then suggest lan1
+// if totallan 1 = totallan2 && > totallan3, then suggest lan1 and lan2
+// if total of all languages is =, then provide link to epicodus.com/blog/what-programming-langauge-should-i-learn (this will also be included in "further reading")
 
 
 
